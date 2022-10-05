639f919887ede5354120acdfb46c5b2346ee2feb
JeppeFoldberg <jeppe_05@msn.com>
2022-10-05 09:50:18 +0200

    working on network

--- Analyse/network.py
+++ Analyse/network.py
@@ -4,4 +4,9 @@ import pandas as pd
 df = pd.read_csv('../Renset data/test_af_nyt_script.csv', index_col=0)
 # %%
 # way of getting only the rows with actual tags
-# test.tag[~pd.isna(test.tag) == True]
+df = df.tag[~pd.isna(test.tag) == True]
+
+#%%
+df.shape
+
+# %%
