0b0e5c742af15d32130269cb2baeba5cbdf5eb0b
JeppeFoldberg <jeppe_05@msn.com>
2022-10-25 09:54:09 +0200

    Deleted unneeded time measuring

--- create_df/html_parser.py
+++ create_df/html_parser.py
@@ -1,25 +1,9 @@
-from functools import wraps
-import time
 from bs4 import BeautifulSoup as bs
 import re
 from dateutil.parser import parse
 import pandas as pd
 import sys
 
-def timeit(func):
-    @wraps(func)
-    def timeit_wrapper(*args, **kwargs):
-        start_time = time.perf_counter()
-        result = func(*args, **kwargs)
-        end_time = time.perf_counter()
-        total_time = end_time - start_time
-        print(f'Function {func.__name__}{args} {kwargs} Took {total_time:.4f} seconds')
-        return result
-
-    return timeit_wrapper
-
-
-# @timeit
 def parse_html(path): 
     with open(path) as f:
         # read file
