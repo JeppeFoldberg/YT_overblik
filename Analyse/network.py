62182114d95fdc3968a0f3a21634456318307e8f
JeppeFoldberg <jeppe_05@msn.com>
2022-10-25 09:59:34 +0200

    renamed network creator; deleted old unused one

--- Analyse/network.py
+++ Analyse/network.py
@@ -1,60 +1,65 @@
 #%% 
+from venv import create
 import pandas as pd
-from nltk import bigrams
-import itertools
-from collections import Counter
-import networkx as nx
-import matplotlib.pyplot as plt
-# %%
-df = pd.read_csv('../Renset data/test_af_nyt_script.csv', index_col=0)
-# %%
-# way of getting only the rows with actual tags
-tags_list = df.tag[~pd.isna(df.tag) == True]
-# for each list split into tags that becomes its own string
-tags_list = [tags.split(',') for tags in tags_list]
+# from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer
+import sys
+#%% 
+# stopwords = stopwords.words('english')
+# print(sys.getsizeof(tags_list))
+
+# tags_list = [tags.split(',') for tags in tag_lists]
+
+# remove stopwords as well! 
+# tags_list = [[word for word in tag_list if word not in stopwords] for tag_list in tags_list] 
+# print(sys.getsizeof(tags_list))
+#%% 
+def create_cooc_matrix(df):
+    '''takes a df, gets the tags and creates a coocurrence matrix'''
+    # way of getting only the rows with actual tags
+    tag_lists = df.tag[~pd.isna(df.tag) == True]
+    no_tags = df.shape[0]-tag_lists.shape[0]
+    pct_no_tags = no_tags / df.shape[0] * 100
+    print(f'{no_tags} videos did not have tags. This amounts to {pct_no_tags:.2f}%')
+    # splits on ',' because we trust the tokens used by creators! 
+    cv = CountVectorizer(token_pattern=r'[^,]*', min_df=0.01, max_df=0.95) # Maybe set min and max df to minimize the size of the network! could be max_df = 0.95, min_df = 0.4
+
+    X = cv.fit_transform(tag_lists)
+    Xc = (X.T * X) # This is the matrix manipulation step
+    Xc.setdiag(0) # We set the diagonals to be zeroes as it's pointless to be 1
+
+    names = cv.get_feature_names_out()
+    df = pd.DataFrame(data = Xc.toarray(), columns = names, index = names)
+
+    return df
+
+#%%
+def create_yearly_plots(df):
+    df['date_watched'] = pd.to_datetime(df['date_watched'], utc=True)
+    df['year_watched'] = df['date_watched'].dt.year 
+    years = df.year_watched.unique()
+
+    yearly_dfs = {}
+    for year in years:
+        year_df = df[df.year_watched == year]
+        year_cooc = create_cooc_matrix(year_df)
+
+        yearly_dfs[str(year)] = year_cooc
+
+    return yearly_dfs
 
 # %%
-terms_bigram = [list(bigrams(tags)) for tags in tags_list]
-# %%
-# Flatten list of bigrams in 
-bigrams = list(itertools.chain(*terms_bigram))
+def main():
+    path_to_watch_history = sys.argv[1]   
+    path_to_network_folder = sys.argv[2]   
 
-# Create counter of words in clean bigrams
-bigram_counts = Counter(bigrams)
+    df = pd.read_csv(path_to_watch_history, index_col=0)
 
-temp = bigram_counts.most_common(200)
-# %%
-bigram_df = pd.DataFrame(bigram_counts.most_common(50), columns=['bigram', 'count'])
-# Create dictionary of bigrams and their counts
-d = bigram_df.set_index('bigram').T.to_dict('records')
-# %%
-G = nx.Graph()
-
-# Create connections between nodes
-for k, v in d[0].items():
-    G.add_edge(k[0], k[1], weight=(v * 10))
+    # cooc_matrix = create_cooc_matrix(df)
+    yearly_dfs = create_yearly_plots(df)
+    for key, value in yearly_dfs.items():
+        value.to_csv(f'{path_to_network_folder}/{key}.csv', sep = ',')
 
 # %%
-fig, ax = plt.subplots(figsize=(10, 8))
-
-pos = nx.spring_layout(G, k=2)
-
-# Plot networks
-nx.draw_networkx(G, pos,
-                 font_size=16,
-                 width=3,
-                 edge_color='grey',
-                 node_color='purple',
-                 with_labels = False,
-                 ax=ax)
-
-# Create offset labels
-for key, value in pos.items():
-    x, y = value[0]+.135, value[1]+.045
-    ax.text(x, y,
-            s=key,
-            bbox=dict(facecolor='red', alpha=0.25),
-            horizontalalignment='center', fontsize=13)
-    
-plt.show()
-# %%
+if __name__ == "__main__":
+    main()
