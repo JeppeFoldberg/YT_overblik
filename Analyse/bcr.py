0a724c48e6bc490dbd445b22183b05b480ea19e4
JeppeFoldberg <jeppe_05@msn.com>
2022-11-07 17:22:24 +0100

    term calls to change amount of channels and cutoff

--- Analyse/bcr.py
+++ Analyse/bcr.py
@@ -5,7 +5,7 @@ import pandas as pd
 import bar_chart_race as bcr
 import sys
 
-def reshape_data(df): 
+def reshape_data(df, cutoff = 10): 
     '''
     Given a watch history dataframe, reshapes the data into a wide format 
     where each row corresponds to a date and each col contains the cumulated sum of views on that channels
@@ -25,7 +25,7 @@ def reshape_data(df):
     df = df.pivot(index='day_watched', columns='channel_title', values='video_title')
 
     # remove channels with less than n views. 
-    df = df[df.columns[df.sum()>10]]
+    df = df[df.columns[df.sum()>cutoff]]
 
     # replace nans with 0
     df = df.fillna(0)
@@ -45,10 +45,16 @@ def create_bcr(df, filename, n_bars = 25, period_length = 100, **kwargs):
 
 def main():
     respondent = sys.argv[1]
+    if len(sys.argv) > 2:
+        n_bars = int(sys.argv[2])
+        cutoff = int(sys.argv[3])
+    else:
+        n_bars = 25
+        cutoff = 10
 
     df = pd.read_csv(f'cleaned_data/{respondent}/history_info_df.csv', index_col=0)
-    df = reshape_data(df)
-    create_bcr(df, f'results/{respondent}/bcr.gif')
+    df = reshape_data(df, cutoff)
+    create_bcr(df, f'results/{respondent}/bcr.gif', n_bars)
 
 
 if __name__ == '__main__':
